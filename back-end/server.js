a75ab8353ffee7df368524fddb47bbe87688352d
pranav <pranavofficial404@gmail.com>
2022-12-23 21:55:31 +0530

    changes

--- back-end/server.js
+++ back-end/server.js
@@ -1,8 +1,22 @@
 const express = require('express')
-const io = require('socket.io')
+const { Server } = require('socket.io')
+const http = require('http')
 const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
+
 
 app.use(express.static('public'))
+
+io.on('connection', (socket) => {
+    socket.on('bar1', (msg) => {
+       io.emit('bar1',msg)
+    })
+    socket.on('bar2', (msg) => {
+        io.emit('bar2',msg)
+    })
+});
+  
 
 app.listen(3000, () => {
     console.log('server is listening on port 3000')
